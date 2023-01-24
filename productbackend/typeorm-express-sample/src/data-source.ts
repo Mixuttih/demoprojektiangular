f004e6fd55d0a18a7a947f291aa6cead9134abf6
Your Name <you@example.com>
2023-01-24 13:38:18 +0200

    credentials to environment variable

--- productbackend/typeorm-express-sample/src/data-source.ts
+++ productbackend/typeorm-express-sample/src/data-source.ts
@@ -1,15 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Product } from "./entity/Product"
+import * as dotenv from "dotenv"
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mongodb",
-    host: "asdasdvasdbsdfb.mongo.cosmos.azure.com",
-    username: "asdasdvasdbsdfb",
-    password: "IpLyfcKKQgfXQV3ZsUiUDQJAwtTDNWhnF4DEOS8b53JfPqyeFnDp6SaJ0dguRPsjUmFmg0qhMe9XACDbanN1mg==",
-    port: 10255,
-    database: "test",
-    ssl: true,
+    host: process.env.host,
+    username: process.env.username,
+    password: process.env.password,
+    port: parseInt(process.env.port),
+    database: process.env.database,
+    ssl: Boolean(process.env.ssl),
     synchronize: true,
     logging: false,
     entities: [Product],
