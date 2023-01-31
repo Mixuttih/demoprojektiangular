99e9e9269ca6c42bdade5909cff837ea49b96a2c
Your Name <you@example.com>
2023-01-31 13:57:05 +0200

    Productinfo site

--- myApp/src/app/productinfo/productinfo.component.ts
+++ myApp/src/app/productinfo/productinfo.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product/product.service';
+import { Product } from '../product/product';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-productinfo',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./productinfo.component.scss']
 })
 export class ProductinfoComponent {
+  public id: string = ""
+  public product: Product = {
+    _id: "undefined",
+    name: "...",
+    description: "...",
+    price: 99.99,
+    category: "Computer"
+  }
+
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router){
+    route.params.subscribe(params => {
+      this.id = params['id'];
+      this.loadData()
+    })
+  }
+
+  loadData(): void {
+    this.productService.readProduct(this.id).subscribe(
+      (data: Product) => {
+        this.product = data
+      }
+    )
+  }
 
 }
